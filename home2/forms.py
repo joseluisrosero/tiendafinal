2c38d61f64049ca209f11ecda9638b39e98936a7
jose rosero <jose.rosero.q@uniautonoma.edu.co>
2024-10-15 18:16:00 -0500

    actualizacion Crud

--- home2/forms.py
+++ home2/forms.py
@@ -1,8 +1,11 @@
-from django import forms
+from django import forms 
+from .forms import*
+from .models import Producto
 
-class contacto_form(forms.Form):
+class agregar_producto_form(forms.ModelForm):
+    class Meta: 
+        model = Producto 
+        fields = '__all__'
 
-    correo =forms.EmailField(widget=forms.TextInput)
-    Titulo =forms.CharField(widget=forms.TextInput)
-    texto =forms.EmailField(widget=forms.Textarea)
+
     
