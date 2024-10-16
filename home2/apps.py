95a1c210615693db14d499ef0ed42c55de266951
jose rosero <jose.rosero.q@uniautonoma.edu.co>
2024-10-16 11:53:16 -0500

    señal postsave basica

--- home2/apps.py
+++ home2/apps.py
@@ -4,3 +4,9 @@ from django.apps import AppConfig
 class Home2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home2'
+
+class Home2Config(AppConfig):
+    name = 'home2'
+
+    def ready(self):
+        import home2.signals
---
+++ home2/signals.py
@@ -0,0 +1,10 @@
+from django.db.models.signals import*
+from django.dispatch import receiver
+from .models import*
+
+@receiver(post_save, sender=Producto)
+def despues_de_guardar(sender, instance, created, **kwargs):
+    if created:
+        print(f'Se ha creado un nuevo producto: {instance.nombre}')
+    else:
+        print(f'Se ha actualizado el producto: {instance.nombre}')
