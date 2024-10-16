bf02ebfbc3a48fefa0fc63a1da040bb3da5835be
jose rosero <jose.rosero.q@uniautonoma.edu.co>
2024-10-15 21:18:03 -0500

    agregar ya funciona

--- home2/urls.py
+++ home2/urls.py
@@ -3,9 +3,9 @@ from .views import*
 
 urlpatterns = [
 
-    path('', vista_home, name='home'),
+    path('home', vista_home, name='home'),
     path('about/',vista_about),
-    path('', vista_funko, name='funko'),
+    path('funko', vista_funko, name='funko'),
     path('contacto/',vista_contacto,name='vista_contacto'),   
     path('lista_producto/',vista_lista_producto),
     path('agregar_producto/',vista_agregar_producto,name='vista_agregar_producto'),
