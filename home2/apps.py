d8c107c98171a07efd5a18c3994e428025554906
jose rosero <jose.rosero.q@uniautonoma.edu.co>
2024-10-16 13:20:43 -0500

    señal postsave basica

--- home2/apps.py
+++ home2/apps.py
@@ -4,9 +4,5 @@ from django.apps import AppConfig
 class Home2Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'home2'
-
-class Home2Config(AppConfig):
-    name = 'home2'
-
     def ready(self):
         import home2.signals
