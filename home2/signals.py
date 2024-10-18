dc6f784e27d002e6ae40028e91b7c4d180cbcbed
jose rosero <jose.rosero.q@uniautonoma.edu.co>
2024-10-17 21:34:01 -0500

    exposicion

--- home2/signals.py
+++ home2/signals.py
@@ -4,28 +4,35 @@ from .models import*
 from django.core.mail import send_mail
 
 @receiver(post_save, sender=Producto)
-def despues_de_guardar(sender, instance, created, **kwargs):
+def enviar_correo_despues_de_guardar(sender, instance, created, **kwargs):
     if created:
-        print(f'Se ha creado un nuevo producto: {instance.nombre}')
+        asunto = 'Nuevo producto creado'
+        mensaje = f'Se ha creado un nuevo producto: {instance.nombre}'
     else:
-        print(f'Se ha actualizado el producto: {instance.nombre}') 
+        asunto = 'Producto actualizado'
+        mensaje = f'Se ha actualizado el producto: {instance.nombre}'
 
-
-        
-
-#post_save.disconnect(despues_de_guardar, sender=Producto)
-
-
-
-
-
-
-"""@receiver(pre_save, sender=Producto)
+    send_mail(
+        asunto,
+        mensaje,
+        'roseroquicazanjoseluis@gmail.com',
+        ['j.lucho9006@gmail.com'])
+    
+@receiver(pre_save, sender=Producto)
 def antes_de_guardar(sender, instance, **kwargs):
     if instance.pk:  # Si la instancia tiene una clave primaria, entonces ya existe en la base de datos
         print(f"El producto {instance.nombre} se va a actualizar")
     else:
         print(f"El producto {instance.nombre} se va a guardar por primera vez")
+
+        
+
+pre_save.disconnect(antes_de_guardar, sender=Producto)
+
+
+
+
+"""
 
 
 @receiver(post_save, sender=Producto)
@@ -45,18 +52,5 @@ def antes_de_eliminar(sender, instance, **kwargs):
 def despues_de_eliminar(sender, instance, **kwargs):
     print(f'Se ha eliminado el producto: {instance.nombre}')
 
-@receiver(post_save, sender=Producto)
-def enviar_correo_despues_de_guardar(sender, instance, created, **kwargs):
-    if created:
-        asunto = 'Nuevo producto creado'
-        mensaje = f'Se ha creado un nuevo producto: {instance.nombre}'
-    else:
-        asunto = 'Producto actualizado'
-        mensaje = f'Se ha actualizado el producto: {instance.nombre}'
 
-    send_mail(
-        asunto,
-        mensaje,
-        'roseroquicazanjoseluis@gmail.com',
-        ['j.lucho9006@gmail.com']
     )"""
