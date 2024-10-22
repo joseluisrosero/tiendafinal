238b5af16b4f5fd7c0f5c5d71ed542f6e774f17b
jose rosero <jose.rosero.q@uniautonoma.edu.co>
2024-10-21 20:16:12 -0500

    cambio base de datos

--- tienda2/settings.py
+++ tienda2/settings.py
@@ -76,6 +76,8 @@ WSGI_APPLICATION = 'tienda2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+""""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
@@ -84,6 +86,13 @@ DATABASES = {
         'PASSWORD' : '',
         'HOST' : 'localhost',
         'PORT': '3306',
+    }
+}
+"""
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
